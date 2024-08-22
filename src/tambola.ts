10b72195e9369b9934cb1f6200ee5d843e337c7d
Swarup Mahapatra <swarup@Swarups-MacBook-Pro.local>
2024-08-22 15:53:33 +0530

    Extract functions for repeating code

--- src/tambola.ts
+++ src/tambola.ts
@@ -44,26 +44,24 @@ export class Game {
 }
 
 export class Ticket {
+  removeUnderscore = (char: "_" | number) => (char !== '_')
+
   constructor(private readonly numbers: Array<Array<number | "_">>) {
     if (numbers.length !== 3) throw new Error("Invalid ticket")
   }
   get topRow() {
-    const removeUnderscore = (char: "_" | number) => (char !== '_')
-    return new Set(this.numbers[0].filter(removeUnderscore));
+    return new Set(this.numbers[0].filter(this.removeUnderscore));
   }
   get middleRow() {
-    const removeUnderscore = (char: "_" | number) => (char !== '_')
-    return new Set(this.numbers[1].filter(removeUnderscore));
+    return new Set(this.numbers[1].filter(this.removeUnderscore));
   }
   get bottomRow() {
-    const removeUnderscore = (char: "_" | number) => (char !== '_')
-    return new Set(this.numbers[2].filter(removeUnderscore));
+    return new Set(this.numbers[2].filter(this.removeUnderscore));
   }
   get allRows() {
-    const removeUnderscore = (char: "_" | number) => (char !== '_')
-    const topRowNumbers = this.numbers[0].filter(removeUnderscore);
-    const middleRowNumbers = this.numbers[1].filter(removeUnderscore)
-    const bottomRowNumbers = this.numbers[2].filter(removeUnderscore)
+    const topRowNumbers = this.numbers[0].filter(this.removeUnderscore);
+    const middleRowNumbers = this.numbers[1].filter(this.removeUnderscore)
+    const bottomRowNumbers = this.numbers[2].filter(this.removeUnderscore)
     const allNumbers = [...topRowNumbers, ...middleRowNumbers, ...bottomRowNumbers]
     return new Set(allNumbers);
   }
@@ -80,10 +78,8 @@ class TopRowClaim implements Claim {
 
   evaluate() {
     const numbersToCheck = this.ticket.topRow
-    const lastAnnouncedNumber = this.numbersAccounced[this.numbersAccounced.length - 1]
-    if (!numbersToCheck.has(lastAnnouncedNumber)) return 'REJECTED'
-    this.numbersAccounced.forEach(num => numbersToCheck.delete(num))
-    return numbersToCheck.size === 0 ? 'ACCEPTED' : 'REJECTED'
+    const check = new Checks(this.numbersAccounced, numbersToCheck)
+    return check.hasLastAnnouncedNumber() && check.hasAllNumbers() ? 'ACCEPTED' : 'REJECTED'
   }
 }
 
@@ -94,10 +90,8 @@ class MiddleRowClaim implements Claim {
 
   evaluate() {
     const numbersToCheck = this.ticket.middleRow
-    const lastAnnouncedNumber = this.numbersAccounced[this.numbersAccounced.length - 1]
-    if (!numbersToCheck.has(lastAnnouncedNumber)) return 'REJECTED'
-    this.numbersAccounced.forEach(num => numbersToCheck.delete(num))
-    return numbersToCheck.size === 0 ? 'ACCEPTED' : 'REJECTED'
+    const check = new Checks(this.numbersAccounced, numbersToCheck)
+    return check.hasLastAnnouncedNumber() && check.hasAllNumbers() ? 'ACCEPTED' : 'REJECTED'
   }
 }
 
@@ -108,10 +102,8 @@ class BottomRowClaim implements Claim {
 
   evaluate() {
     const numbersToCheck = this.ticket.bottomRow
-    const lastAnnouncedNumber = this.numbersAccounced[this.numbersAccounced.length - 1]
-    if (!numbersToCheck.has(lastAnnouncedNumber)) return 'REJECTED'
-    this.numbersAccounced.forEach(num => numbersToCheck.delete(num))
-    return numbersToCheck.size === 0 ? 'ACCEPTED' : 'REJECTED'
+    const check = new Checks(this.numbersAccounced, numbersToCheck)
+    return check.hasLastAnnouncedNumber() && check.hasAllNumbers() ? 'ACCEPTED' : 'REJECTED'
   }
 }
 
@@ -122,10 +114,8 @@ class FullHouseClaim implements Claim {
 
   evaluate() {
     const numbersToCheck = this.ticket.allRows
-    const lastAnnouncedNumber = this.numbersAccounced[this.numbersAccounced.length - 1]
-    if (!numbersToCheck.has(lastAnnouncedNumber)) return 'REJECTED'
-    this.numbersAccounced.forEach(num => numbersToCheck.delete(num))
-    return numbersToCheck.size === 0 ? 'ACCEPTED' : 'REJECTED'
+    const check = new Checks(this.numbersAccounced, numbersToCheck)
+    return check.hasLastAnnouncedNumber() && check.hasAllNumbers() ? 'ACCEPTED' : 'REJECTED'
   }
 }
 
@@ -136,13 +126,36 @@ class EarlyFiveClaim implements Claim {
     private readonly ticket: Ticket) { }
   evaluate() {
     const numbersToCheck = this.ticket.allRows;
-    const lastAnnouncedNumber = this.numbersAccounced[this.numbersAccounced.length - 1]
-    if (!numbersToCheck.has(lastAnnouncedNumber)) return 'REJECTED'
+    const check = new Checks(this.numbersAccounced, numbersToCheck)
+    return check.hasLastAnnouncedNumber() &&
+      check.matchedFirstNnumbers(5) ? 'ACCEPTED' : 'REJECTED'
+  }
+}
 
+class Checks {
+  constructor(
+    private readonly numbersAccounced: number[],
+    private readonly numbersToCheck: Set<number>
+  ) { }
+  hasLastAnnouncedNumber(): Boolean {
+    const lastAnnouncedNumber = this.numbersAccounced[this.numbersAccounced.length - 1]
+    return this.numbersToCheck.has(lastAnnouncedNumber)
+  }
+
+  hasAllNumbers(): Boolean {
+    for (let num of this.numbersToCheck) {
+      if (!this.numbersAccounced.includes(num)) {
+        return false
+      }
+    }
+    return true
+  }
+
+  matchedFirstNnumbers(n: number): Boolean {
     let numbersMatched = 0;
     for (let number of this.numbersAccounced) {
-      if (numbersToCheck.has(number)) numbersMatched++
+      if (this.numbersToCheck.has(number)) numbersMatched++
     }
-    return numbersMatched === 5 ? 'ACCEPTED' : 'REJECTED'
+    return numbersMatched === n
   }
 }
